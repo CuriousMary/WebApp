ad98d36365c018ede21cd34c8f55b34e626eed03
CuriousMary <98719317+CuriousMary@users.noreply.github.com>
2025-02-15 21:37:37 +0000

    fixed cache

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('pwa-cache').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json'
-            ]);
-        })
+      caches.open('my-cache-v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/icon1.png', // Make sure to add the updated icon here
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+  });
+  
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['my-cache-v1']; // Update cache version here
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
     );
-});
+  });
+  
