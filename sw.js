f58ead32eba72f9e28fccd95dd3839b2d9c8c7b8
CuriousMary <98719317+CuriousMary@users.noreply.github.com>
2025-02-15 21:40:52 +0000

    fixed sw

--- sw.js
+++ sw.js
@@ -1,19 +1,45 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('pwa-cache').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json'
-            ]);
-        })
-    );
+var CACHE_NAME = 'webapp';
+var urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+ 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+          
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
     );
 });
