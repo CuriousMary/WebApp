7cc0dde8ee16f32e1994c777eef2f8f9fccf66ff
CuriousMary <98719317+CuriousMary@users.noreply.github.com>
2025-02-18 22:57:10 +0000

    updated register

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,13 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-        .then(() => console.log("Service Worker Registered"))
-        .catch(err => console.log("Service Worker Registration Failed", err));
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+        registration.onupdatefound = () => {
+            const newWorker = registration.installing;
+            newWorker.onstatechange = () => {
+                if (newWorker.state === "installed" && navigator.serviceWorker.controller) {
+                    console.log("New version available. Refreshing...");
+                    window.location.reload(); // Forces refresh when a new service worker is installed
+                }
+            };
+        };
+    }).catch(error => console.log("Service Worker Registration Failed:", error));
 }
